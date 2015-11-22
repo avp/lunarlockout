35ad502da7f1c94155fde4866deade3024422add
Aakash Patel <aakpat6@gmail.com>
2015-11-22 17:18:06 -0500

    Add get_moves and make_move.

--- state.py
+++ state.py
@@ -1,12 +1,12 @@
 class State:
 
-  def __init__(self, positions):
+  def __init__(self, positions, g=0):
     """
     Positions: Dictionary mapping strings ("X", "A", etc) to tuples (row,col).
     e.g. {"X": (0,0), "Y": (2,2), ...}
     """
     self.positions = positions
-    self.g = 0
+    self.g = g
 
   def get_positions(self):
     """
@@ -32,18 +32,42 @@ class State:
   def get_moves(self):
     """
     Returns:
-    List of moves, where a move is of the form (pod, direction)
-    e.g. [("X", "L"), ("A", "D"), ...]
+    List of moves, where a move is of the form (pod, pos)
+    e.g. [("X", (0,1)), ("A", (2,0)), ...]
     """
-    pass
+    DELTAS = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+    occupied = set(self.positions.values())
+    def can_move(pod, dir):
+      (dr,dc) = DELTAS[dir]
+      (r0,c0) = self.positions[pod]
+      for i in xrange(2, 5): # Start at 2 because not moving is pointless
+        (r,c) = (r0 + i*dr, c0 + i*dc)
+        if r < 0 or r > 4 or c < 0 or c > 4:
+          return None
+        if (r,c) in occupied:
+          return (r-dr,c-dc)
+      return None
 
-  def make_move(self, (pod, dir)):
+    moves = []
+    for pod in ('X', 'A', 'B', 'C', 'D', 'E'):
+      for dir in ('U', 'D', 'L', 'R'):
+        move = can_move(pod, dir)
+        if move:
+          moves.append(move)
+    return moves
+
+  def make_move(self, (pod, pos)):
     """
-    Returns a new state where the pod has been moved in direction dir.
+    Returns a NEW state where the pod has been moved in direction dir.
     REQUIRES: (pod,dir) is a valid move.
     """
-    self.g += 1
-    pass
+    positions = {}
+    for a, b in self.positions.iteritems():
+      if pod == a:
+        positions[a] = pos
+      else:
+        positions[a] = b
+    return State(positions, self.g + 1)
 
   def is_solved(self):
     """
