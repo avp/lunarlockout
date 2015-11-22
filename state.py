6f3abe2fba24d3ed73d0c3a5de4d0c243da144a4
Aakash Patel <aakpat6@gmail.com>
2015-11-22 15:37:34 -0500

    Add g value and override == and !=.

--- state.py
+++ state.py
@@ -1,17 +1,33 @@
 class State:
 
-  def __init__(self, positions):
+  def __init__(self, positions, g):
     """
     Positions: Dictionary mapping strings ("X", "A", etc) to tuples (row,col).
     e.g. {"X": (0,0), "Y": (2,2), ...}
     """
     self.positions = positions
+    self.g = g
 
   def get_positions(self):
     """
     Returns dictionary of pods to positions tuple
     """
     return self.positions
+
+  def get_g(self):
+    """
+    Returns number of moves to this state
+    """
+    return self.g
+
+  def __eq__(self, other):
+    return (
+        isinstance(other, self.__class__) and
+        self.positions == other.get_positions()
+        )
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
 
   def get_moves(self):
     """
